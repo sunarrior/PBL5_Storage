f65c5bf629f89790f7bb6299e617c6d26250fe43
nhat <nhat@gmail.com>
2022-06-02 21:37:54 +0700

    update main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+config = {
+    "apiKey": "AIzaSyB3BRNg6k6knElb_LXq37ksMdG1VhQEfdQ",
+    "authDomain": "demofirebasepython.firebaseapp.com",
+    "projectId": "demofirebasepython",
+    "databaseURL": "https://demofirebasepython-default-rtdb.firebaseio.com/",
+    "storageBucket": "demofirebasepython.appspot.com",
+    "messagingSenderId": "248389388826",
+    "appId": "1:248389388826:web:5fd5b09678b0e203ad1fa3",
+    "measurementId": "G-VPNHZP253X"
+}
+ws_ip = 192.168.0.105
+cam_ip = 192.168.0.107
+led_room = "leds/led_living_room"
+
